680050d07ce5befccf0bfd69a4027b84b20f1948
Ola <ohagenbendixen@gmail.com>
2023-01-02 15:08:47 +0100

    Render the gameBoard content

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable arrow-body-style */
 /* eslint-disable no-shadow */
 /* eslint-disable no-unused-vars */
 const gameBoard = (() => {
-  const gameBoard = ['X', 'O', 'X', 'X', 'O', 'O', 'X', 'O', 'X'];
+  const gameBoardArr = ['X', 'O', 'X', 'X', 'O', 'O', 'X', 'O', 'X'];
+
+  const _renderGameBoard = (() => {
+    const gameBoardGrid = document.querySelector('#grid-container');
+    for (let i = 0; i < 9; i++) {
+      console.log(gameBoardGrid);
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      cell.textContent = gameBoardArr[i];
+      gameBoardGrid.appendChild(cell);
+    }
+  })();
+  return { gameBoardArr };
 })();
 
 const makePlayer = (name, selector) => {
   return { name, selector };
 };
 
-const displayController = (() => {
-
-})();
+const displayController = (() => {})();
