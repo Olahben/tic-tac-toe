a61db8dfd0f799a5943eaf2890bcb2098f8c6968
Ola <ohagenbendixen@gmail.com>
2023-01-02 13:50:54 +0100

    Return the players values in the makePlayer function

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+/* eslint-disable arrow-body-style */
 /* eslint-disable no-shadow */
 /* eslint-disable no-unused-vars */
 const gameBoard = (() => {
   const gameBoard = [];
 })();
 
-const makePlayers = (name, selector) => {
-
+const makePlayer = (name, selector) => {
+  return { name, selector };
 };
 
 const displayController = (() => {
