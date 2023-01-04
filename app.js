930958de32f968238a03a121444908357c62f82b
Ola <ohagenbendixen@gmail.com>
2023-01-04 18:04:34 +0100

    Assign a index property to each cell created

--- app.js
+++ app.js
@@ -3,24 +3,37 @@
 /* eslint-disable arrow-body-style */
 /* eslint-disable no-shadow */
 /* eslint-disable no-unused-vars */
+
+let setCellIndex = 0;
+
 const gameBoard = (() => {
   const gameBoardArr = ['', '', '', '', '', '', '', '', ''];
 
   const _renderGameBoard = (() => {
     const gameBoardGrid = document.querySelector('#grid-container');
     for (let i = 0; i < 9; i++) {
-      console.log(gameBoardGrid);
+      // console.log(gameBoardGrid);
       const cell = document.createElement('div');
       cell.classList.add('cell');
       cell.textContent = gameBoardArr[i];
+      cell.index = setCellIndex;
+      setCellIndex++;
       gameBoardGrid.appendChild(cell);
     }
   })();
   return { gameBoardArr };
 })();
 
+
 const makePlayer = (name, selector) => {
   return { name, selector };
 };
 
-const displayController = (() => {})();
+const displayController = (() => {
+  const cells = document.querySelectorAll('.cell')
+  cells.forEach((cell) => {
+    cell.addEventListener('click', () => {
+
+    })
+  })
+})();
