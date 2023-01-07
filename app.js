9b9ada3dc398372b73a68a19311b8d0fb6d10002
Ola <ohagenbendixen@gmail.com>
2023-01-07 15:20:53 +0100

    Place checkForWin function as a sibling for the placeMarker function Inside displayController

--- app.js
+++ app.js
@@ -44,161 +44,161 @@ const displayController = (() => {
         marker = 'X';
       }
     });
+  });
 
-    const checkForWin = cells.forEach((cell) => {
-      cell.addEventListener('click', () => {
-        if (
-          gameBoard.gameBoardArr[0] === 'X' &&
-          gameBoard.gameBoardArr[1] === 'X' &&
-          gameBoard.gameBoardArr[2] === 'X'
-        ) {
-          console.log('X WINS');
-          return 'X WINS';
-        }
-        if (
-          gameBoard.gameBoardArr[0] === 'O' &&
-          gameBoard.gameBoardArr[1] === 'O' &&
-          gameBoard.gameBoardArr[2] === 'O'
-        ) {
-          console.log('O WINS');
-          return 'O WINS';
-        }
-        if (
-          gameBoard.gameBoardArr[3] === 'X' &&
-          gameBoard.gameBoardArr[4] === 'X' &&
-          gameBoard.gameBoardArr[5] === 'X'
-        ) {
-          console.log('X WINS');
-          return 'X WINS';
-        }
-        if (
-          gameBoard.gameBoardArr[3] === 'O' &&
-          gameBoard.gameBoardArr[4] === 'O' &&
-          gameBoard.gameBoardArr[5] === 'O'
-        ) {
-          console.log('O WINS');
-          return 'O WINS';
-        }
-        if (
-          gameBoard.gameBoardArr[6] === 'X' &&
-          gameBoard.gameBoardArr[7] === 'X' &&
-          gameBoard.gameBoardArr[8] === 'X'
-        ) {
-          console.log('X WINS');
-          return 'X WINS';
-        }
-        if (
-          gameBoard.gameBoardArr[6] === 'O' &&
-          gameBoard.gameBoardArr[7] === 'O' &&
-          gameBoard.gameBoardArr[8] === 'O'
-        ) {
-          console.log('O WINS');
-          return 'O WINS';
-        }
-        if (
-          gameBoard.gameBoardArr[1] === 'X' &&
-          gameBoard.gameBoardArr[4] === 'X' &&
-          gameBoard.gameBoardArr[7] === 'X'
-        ) {
-          console.log('X WINS');
-          return 'X WINS';
-        }
-        if (
-          gameBoard.gameBoardArr[1] === 'O' &&
-          gameBoard.gameBoardArr[4] === 'O' &&
-          gameBoard.gameBoardArr[7] === 'O'
-        ) {
-          console.log('O WINS');
-          return 'O WINS';
-        }
-        if (
-          gameBoard.gameBoardArr[0] === 'X' &&
-          gameBoard.gameBoardArr[3] === 'X' &&
-          gameBoard.gameBoardArr[6] === 'X'
-        ) {
-          console.log('X WINS');
-          return 'X WINS';
-        }
-        if (
-          gameBoard.gameBoardArr[0] === 'O' &&
-          gameBoard.gameBoardArr[3] === 'O' &&
-          gameBoard.gameBoardArr[6] === 'O'
-        ) {
-          console.log('O WINS');
-          return 'O WINS';
-        }
-        if (
-          gameBoard.gameBoardArr[2] === 'X' &&
-          gameBoard.gameBoardArr[5] === 'X' &&
-          gameBoard.gameBoardArr[8] === 'X'
-        ) {
-          console.log('X WINS');
-          return 'X WINS';
-        }
-        if (
-          gameBoard.gameBoardArr[2] === 'O' &&
-          gameBoard.gameBoardArr[5] === 'O' &&
-          gameBoard.gameBoardArr[8] === 'O'
-        ) {
-          console.log('O WINS');
-          return 'O WINS';
-        }
-        if (
-          gameBoard.gameBoardArr[0] === 'X' &&
-          gameBoard.gameBoardArr[4] === 'X' &&
-          gameBoard.gameBoardArr[8] === 'X'
-        ) {
-          console.log('X WINS');
-          return 'X WINS';
-        }
-        if (
-          gameBoard.gameBoardArr[0] === 'O' &&
-          gameBoard.gameBoardArr[4] === 'O' &&
-          gameBoard.gameBoardArr[8] === 'O'
-        ) {
-          console.log('O WINS');
-          return 'O WINS';
-        }
-        if (
-          gameBoard.gameBoardArr[2] === 'X' &&
-          gameBoard.gameBoardArr[4] === 'X' &&
-          gameBoard.gameBoardArr[6] === 'X'
-        ) {
-          console.log('X WINS');
-          return 'X WINS';
-        }
-        if (
-          gameBoard.gameBoardArr[2] === 'O' &&
-          gameBoard.gameBoardArr[4] === 'O' &&
-          gameBoard.gameBoardArr[6] === 'O'
-        ) {
-          console.log('O WINS');
-          return 'O WINS';
-        }
-        if (
-          gameBoard.gameBoardArr[0] === 'X' ||
-          gameBoard.gameBoardArr[0] === 'O' &&
-          gameBoard.gameBoardArr[1] === 'X' ||
-          gameBoard.gameBoardArr[1] === 'O' &&
-          gameBoard.gameBoardArr[2] === 'X' ||
-          gameBoard.gameBoardArr[2] === 'O' &&
-          gameBoard.gameBoardArr[3] === 'X' ||
-          gameBoard.gameBoardArr[3] === 'O' &&
-          gameBoard.gameBoardArr[4] === 'X' ||
-          gameBoard.gameBoardArr[4] === 'O' &&
-          gameBoard.gameBoardArr[5] === 'X' ||
-          gameBoard.gameBoardArr[5] === 'O' &&
-          gameBoard.gameBoardArr[6] === 'X' ||
-          gameBoard.gameBoardArr[6] === 'O' &&
-          gameBoard.gameBoardArr[7] === 'X' ||
-          gameBoard.gameBoardArr[7] === 'O' &&
-          gameBoard.gameBoardArr[8] === 'X' ||
-          gameBoard.gameBoardArr[8] === 'O'
-        ) {
-          console.log('TIE')
-          return 'TIE';
-        }
-      });
+  const checkForWin = cells.forEach((cell) => {
+    cell.addEventListener('click', () => {
+      if (
+        gameBoard.gameBoardArr[0] === 'X' &&
+        gameBoard.gameBoardArr[1] === 'X' &&
+        gameBoard.gameBoardArr[2] === 'X'
+      ) {
+        console.log('X WINS');
+        return 'X WINS';
+      }
+      if (
+        gameBoard.gameBoardArr[0] === 'O' &&
+        gameBoard.gameBoardArr[1] === 'O' &&
+        gameBoard.gameBoardArr[2] === 'O'
+      ) {
+        console.log('O WINS');
+        return 'O WINS';
+      }
+      if (
+        gameBoard.gameBoardArr[3] === 'X' &&
+        gameBoard.gameBoardArr[4] === 'X' &&
+        gameBoard.gameBoardArr[5] === 'X'
+      ) {
+        console.log('X WINS');
+        return 'X WINS';
+      }
+      if (
+        gameBoard.gameBoardArr[3] === 'O' &&
+        gameBoard.gameBoardArr[4] === 'O' &&
+        gameBoard.gameBoardArr[5] === 'O'
+      ) {
+        console.log('O WINS');
+        return 'O WINS';
+      }
+      if (
+        gameBoard.gameBoardArr[6] === 'X' &&
+        gameBoard.gameBoardArr[7] === 'X' &&
+        gameBoard.gameBoardArr[8] === 'X'
+      ) {
+        console.log('X WINS');
+        return 'X WINS';
+      }
+      if (
+        gameBoard.gameBoardArr[6] === 'O' &&
+        gameBoard.gameBoardArr[7] === 'O' &&
+        gameBoard.gameBoardArr[8] === 'O'
+      ) {
+        console.log('O WINS');
+        return 'O WINS';
+      }
+      if (
+        gameBoard.gameBoardArr[1] === 'X' &&
+        gameBoard.gameBoardArr[4] === 'X' &&
+        gameBoard.gameBoardArr[7] === 'X'
+      ) {
+        console.log('X WINS');
+        return 'X WINS';
+      }
+      if (
+        gameBoard.gameBoardArr[1] === 'O' &&
+        gameBoard.gameBoardArr[4] === 'O' &&
+        gameBoard.gameBoardArr[7] === 'O'
+      ) {
+        console.log('O WINS');
+        return 'O WINS';
+      }
+      if (
+        gameBoard.gameBoardArr[0] === 'X' &&
+        gameBoard.gameBoardArr[3] === 'X' &&
+        gameBoard.gameBoardArr[6] === 'X'
+      ) {
+        console.log('X WINS');
+        return 'X WINS';
+      }
+      if (
+        gameBoard.gameBoardArr[0] === 'O' &&
+        gameBoard.gameBoardArr[3] === 'O' &&
+        gameBoard.gameBoardArr[6] === 'O'
+      ) {
+        console.log('O WINS');
+        return 'O WINS';
+      }
+      if (
+        gameBoard.gameBoardArr[2] === 'X' &&
+        gameBoard.gameBoardArr[5] === 'X' &&
+        gameBoard.gameBoardArr[8] === 'X'
+      ) {
+        console.log('X WINS');
+        return 'X WINS';
+      }
+      if (
+        gameBoard.gameBoardArr[2] === 'O' &&
+        gameBoard.gameBoardArr[5] === 'O' &&
+        gameBoard.gameBoardArr[8] === 'O'
+      ) {
+        console.log('O WINS');
+        return 'O WINS';
+      }
+      if (
+        gameBoard.gameBoardArr[0] === 'X' &&
+        gameBoard.gameBoardArr[4] === 'X' &&
+        gameBoard.gameBoardArr[8] === 'X'
+      ) {
+        console.log('X WINS');
+        return 'X WINS';
+      }
+      if (
+        gameBoard.gameBoardArr[0] === 'O' &&
+        gameBoard.gameBoardArr[4] === 'O' &&
+        gameBoard.gameBoardArr[8] === 'O'
+      ) {
+        console.log('O WINS');
+        return 'O WINS';
+      }
+      if (
+        gameBoard.gameBoardArr[2] === 'X' &&
+        gameBoard.gameBoardArr[4] === 'X' &&
+        gameBoard.gameBoardArr[6] === 'X'
+      ) {
+        console.log('X WINS');
+        return 'X WINS';
+      }
+      if (
+        gameBoard.gameBoardArr[2] === 'O' &&
+        gameBoard.gameBoardArr[4] === 'O' &&
+        gameBoard.gameBoardArr[6] === 'O'
+      ) {
+        console.log('O WINS');
+        return 'O WINS';
+      }
+      if (
+        gameBoard.gameBoardArr[0] === 'X' ||
+        gameBoard.gameBoardArr[0] === 'O' &&
+        gameBoard.gameBoardArr[1] === 'X' ||
+        gameBoard.gameBoardArr[1] === 'O' &&
+        gameBoard.gameBoardArr[2] === 'X' ||
+        gameBoard.gameBoardArr[2] === 'O' &&
+        gameBoard.gameBoardArr[3] === 'X' ||
+        gameBoard.gameBoardArr[3] === 'O' &&
+        gameBoard.gameBoardArr[4] === 'X' ||
+        gameBoard.gameBoardArr[4] === 'O' &&
+        gameBoard.gameBoardArr[5] === 'X' ||
+        gameBoard.gameBoardArr[5] === 'O' &&
+        gameBoard.gameBoardArr[6] === 'X' ||
+        gameBoard.gameBoardArr[6] === 'O' &&
+        gameBoard.gameBoardArr[7] === 'X' ||
+        gameBoard.gameBoardArr[7] === 'O' &&
+        gameBoard.gameBoardArr[8] === 'X' ||
+        gameBoard.gameBoardArr[8] === 'O'
+      ) {
+        console.log('TIE')
+        return 'TIE';
+      }
     });
   });
 })();
